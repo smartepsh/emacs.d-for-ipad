fa3312a6fae5f08fc5791ae3c30955eba19bf12a
Kenton@IPP <smartepsh@gmail.com>
2020-06-20 21:59:25 +0800

    feat: setup avy

--- configs/init-ivy.el
+++ configs/init-ivy.el
@@ -59,4 +59,15 @@
   :config
   (setq wgrep-auto-save-buffer t))
 
+(use-package avy
+  :commands (avy-goto-char-2 avy-goto-line)
+  :config
+  (setq avy-all-windows nil
+        avy-background t)
+  :general
+  (common-leader
+    "j" '(:ignore, t :which-key "jumping")
+    "jj" 'avy-goto-char-2
+    "jl" 'avy-goto-line))
+
 (provide 'init-ivy)
